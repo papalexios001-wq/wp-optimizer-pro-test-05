87fc5552b605fccced402c954aae9aab19756c24
papalexios001-wq <papalexios001@gmail.com>
2026-01-17 23:01:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@lib': fileURLToPath(new URL('./lib', import.meta.url)),
-      '@types': fileURLToPath(new URL('./types', import.meta.url)),
     },
   },
   build: {
